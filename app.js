4dd18fd8a1fa2a9297c70b2d0eae79bc2c8b952b
Vitalij Fedorka <vitaljchik@gmail.com>
2019-05-09 10:01:29 +0300

    add headers, check 'options' method

--- app.js
+++ app.js
@@ -20,6 +20,28 @@ const router = express.Router();
 app.use(urlencodedParser);
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, x-access-token, Content-Type, Accept'
+  );
+  next();
+});
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(logger('dev'));
 
 router.use((err, req, res, next) => {
