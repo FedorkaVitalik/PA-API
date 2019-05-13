c14aac22388ae8d04ada99372b3220bc0d7b8f1b
Vitalij Fedorka <vitaljchik@gmail.com>
2019-05-13 19:05:28 +0300

    follow route

--- routes/follow.js
+++ routes/follow.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable func-names */
@@ -19,6 +20,25 @@ module.exports = function () {
   const router = express.Router();
 
   router.use(middlewareAuth.isAuthorized());
+
+  router.get('/users', async (req, res) => {
+    const users = await User.find({});
+
+    const usersData = [];
+    for (const userObject of users) {
+      if (!userObject._id.equals(req.userId)) {
+        usersData.push({
+          _id: userObject._id,
+          fname: userObject.fname,
+          lname: userObject.lname,
+          followers: userObject.followers,
+          following: userObject.following
+        });
+      }
+    }
+
+    res.status(200).json(usersData);
+  });
 
   router.patch('/follow/:id', async (req, res) => {
     const { id } = req.params;
@@ -81,6 +101,44 @@ module.exports = function () {
         }
       }
     }
+
+    res.status(200).json('Success');
+  });
+
+  router.patch('/unFollow/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findById({ _id: req.userId });
+
+    if (user.isBlocked === true) {
+      throw new CustomError(errorMessages.ACCESS_CLOSED, 400);
+    }
+
+    const bloggerId = ObjectId(id);
+
+    await User.findByIdAndUpdate(
+      {
+        _id: req.userId
+      },
+      {
+        $pull: {
+          following: bloggerId
+        }
+      },
+      { safe: true, upsert: true }
+    );
+
+    await User.findByIdAndUpdate(
+      {
+        _id: bloggerId
+      },
+      {
+        $pull: {
+          followers: req.userId
+        }
+      },
+      { safe: true, upsert: true }
+    );
 
     res.status(200).json('Success');
   });
